53faddea0d8d9607291f2e6b2c3246cb74dd938f
lilianaCortes1901 <liliana.cortes@bruins.belmont.edu>
2024-02-15 22:54:01 -0600

    Pushing all Java Files

---
+++ src/ArrayList.java
@@ -0,0 +1,21 @@
+//Q2 - Array-based List Class
+//stores book instances
+
+public class AList {
+    private int[] arrayData;
+    private int maxSize;
+
+    //empty constructor
+    public AList(int newMaxSize){
+        maxSize = newMaxSize;
+    }
+
+    //void listAdd(Book p)
+    public void listAdd(Book p){
+        
+    }
+
+    //void listRemove(int pos)
+
+    //String toString()
+}
